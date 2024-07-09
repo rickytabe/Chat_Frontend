f6dc296268c1ef7985df1e21a7054f6e3795b2f7
rickytabe <rickytabe2@gmail.com>
2024-07-09 07:14:15 +0100

    Revised

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/rickytabe/Chat_Backend.git/', // Add this line
+  base: '/Chat_Frontend/', // Add this line
 })
